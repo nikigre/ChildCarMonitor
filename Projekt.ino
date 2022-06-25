974413433bb2fdde20b1adaeb08ed31ddd359326
nikigre <nik@nikigre.si>
2022-06-25 14:59:31 +0200

    Added comments

--- Projekt.ino
+++ Projekt.ino
@@ -2,40 +2,46 @@
 
 #define DEBUG true
 
+//Variables for setting up the child car monitor
 #if DEBUG == true
-  #define TimerDelay 1000 * 2 // seconds
+  #define TimerDelay 1000 * 2 // check sensors at X seconds
   #define WarningTemp 25 //25 °C is too much
   #define TempertureAlarm 10 //how many times TimerDelay must pass that we signal alarm
   #define MaxWarningTempCounter 2 * TempertureAlarm //MAX temp counter
 #else
-  #define TimerDelay 1000 * 10 // seconds
+  #define TimerDelay 1000 * 10 // check sensors at X seconds
   #define WarningTemp 35 //35 °C is too much
   #define TempertureAlarm 30 //how many times TimerDelay must pass that we signal alarm
   #define MaxWarningTempCounter 2 * TempertureAlarm //MAX temp counter
 #endif
 
 
+//Temperature sensor parameters
 #define DHTPIN 6
 #define DHTTYPE DHT11
 
+//Buzzer parameters
 #define BuzzerPin 7
 
+//Reset button parameters
 #define ResetButton 2
 
 DHT dht(DHTPIN, DHTTYPE);
 
-char buff[100];
-char temperatureString[6];
+char buff[100]; //For sprintf method
+char temperatureString[6]; //For displaying temperature as string
 
-int timer;
-int warningTempCounter = 0;
+int timer; //Timer that counts loop()
+int warningTempCounter = 0; //How many times the temperature was above limit
 
 
 void setup() {
   Serial.begin(9600);
+  
   pinMode(BuzzerPin, OUTPUT);
   pinMode(ResetButton, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(ResetButton), resetData, CHANGE);
+  
+  attachInterrupt(digitalPinToInterrupt(ResetButton), resetData, CHANGE); //We make an interrupt on pin so we can restart all parameters on click
   
   dht.begin();
 }
@@ -43,7 +49,7 @@ void setup() {
 void loop() {
   sprintf(buff, "%d\twarningTempCounter > TempertureAlarm \t %d > %d", timer, warningTempCounter, TempertureAlarm);
   Serial.println(buff);
-  if(warningTempCounter > TempertureAlarm) {
+  if(warningTempCounter > TempertureAlarm) { //Check if we need to produse an alarm
     alarm();
   } else {
     digitalWrite(BuzzerPin, LOW);
@@ -53,14 +59,15 @@ void loop() {
   
   sprintf(buff, "%d\ttemperature >= WarningTemp \t %s >= %d", timer, temperatureString, WarningTemp);
   Serial.println(buff);
-  if(temperature >= WarningTemp) {
+  if(temperature >= WarningTemp) { //Check if temperature is above alowed limit
     if(warningTempCounter != MaxWarningTempCounter)
       warningTempCounter++;
   } else {
     if(warningTempCounter > 0)
       warningTempCounter--;
   }
-  
+
+  //Delay checking
   delay(TimerDelay);
   timer++;
   Serial.println(" ");
